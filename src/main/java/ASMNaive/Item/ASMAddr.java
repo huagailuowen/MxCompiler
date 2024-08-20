5b7890f3b55766f31e528b97300489c6a9a033f3
huagailuowen <2146964802@qq.com>
2024-08-20 21:45:05 +0800

    Save Commit: Finished ASM

--- src/main/java/ASMNaive/Item/ASMAddr.java
+++ src/main/java/ASMNaive/Item/ASMAddr.java
@@ -4,10 +4,17 @@ package ASMNaive.Item;
 public class ASMAddr {
   protected ASMReg base;
   protected int offset;
+  protected String label;
+  //have label means it is a global variable
 
   public ASMAddr(ASMReg base, int offset) {
     this.base = base;
     this.offset = offset;
+  }
+  public ASMAddr(String label){
+    this.base = null;
+    this.offset = 0;
+    this.label = label;
   }
   @Override
   public String toString() {
